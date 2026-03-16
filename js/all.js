5cea37c95adeab73158fe0becdf4ba704b2c3ed8
RIO WEI <wutony0713@gmail.com>
2026-03-16 22:51:47 +0800

    feat:刪除不要內容

--- js/all.js
+++ js/all.js
@@ -24,39 +24,3 @@ if (window.ResizeObserver && thing40_4) {
     const observer = new ResizeObserver(updateThing41Margin)
     observer.observe(thing40_4)
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// 彈跳視窗
-function popUpShow(showElement) {
-    document.body.style.overflow = "hidden";
-    const show = document.querySelector(showElement);
-    show.classList.add("popControl--active");
-    document.body.style.overflow = "hidden";
-    show.style.overflowY = "scroll";
-}
-
-function popUpClose(showElement) {
-    document.body.style.overflow = "visible";
-    const show = document.querySelector(showElement);
-    show.classList.remove("popControl--active");
-    document.body.style.overflow = "visible";
-    show.style.overflowY = "hidden";
-}
